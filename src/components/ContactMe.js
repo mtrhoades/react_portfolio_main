ca68dfd822453ff78311d670a91a955b336a81f3
root <mtrhoades0727@gmail.com>
2022-04-26 16:54:39 -1000

    added projects and contact components with bootstrap

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -1,7 +1,38 @@
+// IMPORTS
 import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
+// FUNCTIONAL COMPONENT
 export default function ContactMe() {
+    // VANILLA JAVASCRIPT SECTION
+    const wrapperStyle = { display: 'flex', flexDirection: 'column', alignItems: 'center' }
+
+    // JSX SECTION
   return (
-    <div>ContactMe</div>
+    <div style = {wrapperStyle}>
+        <h2>CONTACT ME</h2>
+        <Form>
+            <Form.Group className="mb-3" controlId="formBasicName">
+                <Form.Control type="name" placeholder="Name" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicSubject">
+                <Form.Control type="subject" placeholder="Subject" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicMessage">
+                <Form.Control type="message" placeholder="Message" />
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
+        </Form>
+    </div>
   )
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -15,15 +15,14 @@ export default function NavBar() {
   return (
     <Navbar style ={{backgroundColor: '#04151F'}} variant="dark">
       <Container>
-        <Navbar.Brand href="#home">Matthew Rhoades</Navbar.Brand>
+        <Navbar.Brand href="#home">MATTHEW RHOADES</Navbar.Brand>
         <Nav className="me-auto">
-          <Nav.Link href="#home">About</Nav.Link>
-          <Nav.Link href="#features">Projects</Nav.Link>
-          <Nav.Link href="#pricing">Contact</Nav.Link>
+          <Nav.Link href="#about">About</Nav.Link>
+          <Nav.Link href="#projects">Projects</Nav.Link>
+          <Nav.Link href="#contact">Contact</Nav.Link>
         </Nav>
       </Container>
     </Navbar>
-
   )
 }
 
